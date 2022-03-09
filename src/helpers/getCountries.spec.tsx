68af93c098d014ed6dc2ce3ebe2e56f5e18e390d
guzdziac55 <guzdziac55@gmail.com>
2022-03-09 18:41:13 +0100

    helper functions simple unit tests done

--- src/helpers/getCountries.spec.tsx
+++ src/helpers/getCountries.spec.tsx
@@ -14,10 +14,90 @@ interface ContinentData {
   continents: Continent[];
 }
 
-const dupa = {
-  continent: [],
+const mockData: ContinentData = {
+  continents: [
+    {
+      name: "Africa",
+      code: "AF",
+      countries: [
+        {
+          name: "Angola",
+          code: "AO",
+        },
+        {
+          name: "Burkina Faso",
+          code: "BF",
+        },
+      ],
+    },
+    {
+      name: "Asia",
+      code: "AS",
+      countries: [
+        {
+          name: "United Arab Emirates",
+          code: "AE",
+        },
+        {
+          name: "Afghanistan",
+          code: "AF",
+        },
+        {
+          name: "Armenia",
+          code: "AM",
+        },
+      ],
+    },
+  ],
 };
 
+const mockResult = [
+  {
+    name: "Angola",
+    code: "AO",
+  },
+  {
+    name: "Burkina Faso",
+    code: "BF",
+  },
+  {
+    name: "United Arab Emirates",
+    code: "AE",
+  },
+  {
+    name: "Afghanistan",
+    code: "AF",
+  },
+  {
+    name: "Armenia",
+    code: "AM",
+  },
+];
+
 describe("getCountries function return", () => {
-  it.skip("specyfic countries", () => {});
+  it("all countries from input data", () => {
+    expect(getCountries(mockData, "").length).toEqual(5);
+  });
+  it("all countries from input data", () => {
+    expect(getCountries(mockData, "").length).toEqual(5);
+  });
+  it("countries from continent Asia arg: AS ", () => {
+    expect(getCountries(mockData, "AS").length).toEqual(3);
+  });
+
+  it("countries from continent Africa arg: AF ", () => {
+    expect(getCountries(mockData, "AF").length).toEqual(2);
+  });
+
+  it("all countries objects ", () => {
+    expect(getCountries(mockData, "")).toEqual(mockResult);
+  });
+
+  it("empty array", () => {
+    expect(getCountries(mockData, "xxxx")).toEqual([]);
+  });
+
+  it("no data ", () => {
+    expect(getCountries(mockData, "xxx").length).toEqual(0);
+  });
 });
